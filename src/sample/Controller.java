0fc8e3aea7eb43aa150bd44df0aedadcb44ee62b
AlexandraCristetiu <alexandra.cristetiu@gmail.com>
2019-05-27 15:01:51 +0300

    Sqlite done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,7 @@
 package sample;
 
 public class Controller {
+    public void initialize() {
+        //tabPane.getSelectionModel().select(tabGame);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,8 @@
 package sample;
 
+import helper.ApplicationConstants;
+import helper.SqliteWrapper;
+import helper.Utility;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,13 +14,17 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Test Adventure Game");
+        primaryStage.setScene(new Scene(root, 800, 700));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
+        Utility.createDirectory(ApplicationConstants.APP_FOLDER_DATA_PATH);
+        SqliteWrapper sqliteWrapper = new SqliteWrapper();
+        sqliteWrapper.createAllTables();
         launch(args);
     }
 }
